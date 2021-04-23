d3711a0a90148160c327f9100c72be25855c3e7f
liangming.zhang <liangming.zhang>
2021-04-23 14:22:54 +0800

    ltts-common 公共异常，公共接口枚举

---
+++ ltts-common/ltts-common-core/src/main/java/com/lming/ltts/common/core/entity/IdEntity.java
@@ -0,0 +1,13 @@
+package com.lming.ltts.common.core.entity;
+
+import java.io.Serializable;
+
+/**
+ * Author: liangming.zhang
+ * Date : 2021/4/23 - 14:18
+ * Description:
+ */
+public interface IdEntity extends Serializable {
+
+
+}
